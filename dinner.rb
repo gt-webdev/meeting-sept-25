6a89a9db9488682152476d2d9e9191fdd09ccde0
Jarvis Johnson <magicjarvis@gmail.com>
2012-09-25 20:25:56 -0400

    Add unordered list of food

--- dinner.rb
+++ dinner.rb
@@ -2,19 +2,19 @@ require 'sinatra'
 
 $food = ['chocolate', 'tv dinner', 'cereal', 'hot dogs', 'veal', 'pizza']
 
-def dinner
-  $food[rand($food.size)]
-end
-
 get '/dinner' do
-  erb :index, locals: { dinner: dinner }
+  erb :index, locals: {dinner: dinner}
 end
 
 get '/add_dinner' do
-  erb :new, locals: { food: $food }
+  erb :new, locals: {array: $food}
 end
 
 post '/add_dinner' do
   $food << params[:dinner]
   redirect to('/add_dinner')
+end
+
+def dinner
+  $food[rand($food.size)]
 end
